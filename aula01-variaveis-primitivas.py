abdd482dac4bc7979ebeb30c8a241a422be1ed74
Paiva <danpaiva@live.com>
2020-09-21 12:33:46 -0300

    aula 01

---
+++ aula01-variaveis-primitivas.py
@@ -0,0 +1,22 @@
+nome = "Daniel Paiva"
+idade = 27
+altura = 1.71
+tutor = False
+aluno = True
+trabalhando = None
+
+print(nome)
+print(idade)
+print(altura)
+print(tutor)
+print(aluno)
+print(trabalhando)
+
+nova_idade = idade + 30
+nome_cidade = nome + " Belo Horizonte"
+
+print(nova_idade)
+print(nome_cidade)
+
+altura_sem_tenis = altura - 0.05
+print(altura_sem_tenis)
